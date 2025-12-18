503fb4850275c252fd80fb7ab8e05b772cf0fc1c
Martina Mohsen <martinatnmr@gmail.com>
2025-12-18 12:30:34 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById("userForm");
+const tree = document.getElementById("tree");
+const userName = document.getElementById("userName");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value
+  };
+  userName.textContent = data.name;
+  tree.classList.remove("hidden");
+
+ 
+  fetch("https://formsubmit.co/ajax/George.safwat.saadallah@gmail.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Email sent successfully", data);
+  })
+  .catch(error => {
+    console.error("Error sending email", error);
+  });
+});
